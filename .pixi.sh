c5bf1806c355a2fd3a7493312011b62938ba5f8f
pablovela5620 <pablovela5620@gmail.com>
2024-10-23 14:30:51 -0500

    migrate from pixi.toml to pyproject.toml

---
+++ .pixi.sh
@@ -0,0 +1,3 @@
+export EIGEN_INCLUDE_DIR="/home/pablo/0Dev/personal/mini-dpvo/.pixi/envs/default/include/eigen3"
+export SPACE_ID="pablovela5620/mini-dpvo-zero"
+export WHL_PATH="mini_dpvo-0.1.1-cp311-cp311-linux_x86_64.whl"
